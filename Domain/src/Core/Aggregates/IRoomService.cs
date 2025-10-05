16f10d2d248eeb53de0732784f61d9da5493fce8
feschenko-nelya <feschenko_n_s@list.ru>
2025-10-05 18:22:32 +0300

    Сущность: номер гостиницы

---
+++ Domain/src/Core/Aggregates/IRoomService.cs
@@ -0,0 +1,18 @@
+﻿
+using Core.Entities;
+
+namespace Domain.src.Core.Aggregates
+{
+    /// <summary>
+    /// Менеджер номеров
+    /// </summary>
+    internal interface IRoomService
+    {
+        public void Add(Room room);
+        public void RemoveRoom(long roomId);
+        public void RemoveRooms(long hotelId);
+        public void UpdateRoom(Room room);
+        public Room getRoom(long roomId);
+        public IReadOnlyList<Room> getHotelRooms(long hotelId);
+    }
+}
