81d81c0883f29e954dfd4c06436ff583c5a29494
feschenko-nelya <feschenko_n_s@list.ru>
2025-10-26 19:47:56 +0300

    Photos rep and service

---
+++ Domain/src/Core/HotelService/Repositories/IPhotosRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using Domain.src.Core.HotelService.Entities;
+
+namespace Domain.src.Core.HotelService.Repositories
+{
+    internal interface IPhotosRepository
+    {
+        public void Add(Photo photo);
+        public void Remove(long photoId);
+        public void Update(Photo photo);
+        public Photo Get(long photoId);
+        public Photo Get(long hotelId, long? roomId);
+
+        public IReadOnlyList<Photo> GetAllByHotel(long hotelId);
+        public IReadOnlyList<Photo> GetAllByRoom(long roomId);
+    }
+}
---
+++ Domain/src/Core/HotelService/Services/IPhotosService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.src.Core.HotelService.Entities;
+
+namespace Domain.src.Core.HotelService.Services
+{
+    internal interface IPhotosService
+    {
+        public void Add(Photo room);
+        public void Remove(long photoId);
+        public void Update(Photo photo);
+        public Photo Get(long photoId);
+        public Photo Get(long hotelId, long? roomId);
+
+        public IReadOnlyList<Photo> GetAllByHotel(long hotelId);
+        public IReadOnlyList<Photo> GetAllByRoom(long roomId);
+    }
+}
