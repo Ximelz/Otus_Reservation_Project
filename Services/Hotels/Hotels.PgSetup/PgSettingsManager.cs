5ecc99d0b802acb88f71cc4098f6cd0bfc9f54ae
feschenko-nelya <feschenko_n_s@list.ru>
2026-01-07 22:19:40 +0300

    Настройки подключения к БД PostgreSQL.

---
+++ Services/Hotels/Hotels.PgSetup/PgSettingsManager.cs
@@ -0,0 +1,128 @@
+﻿using System.Text;
+
+namespace Hotels.PgSetup
+{
+    public class PgSettingsManager
+    {
+        public string UserId { get; set; } = "postgres";
+        public string Password { get; set; } = "";
+        public string Host { get; set; } = "127.0.0.1";
+        public int Port { get; set; } = 5432;
+        public string DatabaseName { get; set; } = "Hotels";
+
+        private static string ENV_VAR_NAME = "Otus-Reservation-hotels";
+        private static string ENV_VARS_SEPARATOR = "#$%";
+        private static string ENV_VAR_SEPARATOR = "=";
+        private Dictionary<string, string> _settings = new();
+
+
+        public PgSettingsManager()
+        {
+            SetDefaultSettings();
+        }
+
+        private void SetDefaultSettings()
+        {
+            _settings.Clear();
+
+            _settings["userid"] = "postgres";
+            _settings["password"] = "";
+            _settings["host"] = "127.0.0.1";
+            _settings["port"] = "5432";
+            _settings["databasename"] = "hotels";
+        }
+
+
+        public void SetUserId(string userId)
+        {
+            _settings["userid"] = userId;
+        }
+
+        public void SetHost(string host)
+        {
+            _settings["host"] = host;
+        }
+
+        public void SetPort(string port)
+        {
+            _settings["port"] = port;
+        }
+
+        public void SetPassword(string password)
+        {
+            _settings["password"] = password;
+        }
+
+        public void SetDatabaseName(string databaseName)
+        {
+            _settings["databasename"] = databaseName;
+        }
+
+        public void ReadSettings()
+        {
+            _settings.Clear();
+
+            string? settings = Environment.GetEnvironmentVariable(ENV_VAR_NAME, EnvironmentVariableTarget.User);
+            
+            if (string.IsNullOrEmpty(settings))
+            {
+                SetDefaultSettings();
+
+                Console.WriteLine("Hotels' service: PostgreSQL settings is empty.");
+
+                return;
+            }
+
+            foreach (var setting in settings.Split(ENV_VARS_SEPARATOR))
+            {
+                string[] vals = setting.Split(ENV_VAR_SEPARATOR);
+
+                if (vals.Length == 0)
+                {
+                    continue;
+                }
+                else if (vals.Length != 2)
+                {
+                    Console.WriteLine($"Hotels' service: wrong format of PostgreSQL setting '{setting}'");
+                    continue;
+                }
+
+                _settings[vals[0]] = vals[1];
+            }
+        }
+
+        public void WriteSettings()
+        {
+            StringBuilder settings = new();
+
+            foreach (var i in _settings)
+            {
+                if (settings.Length > 0)
+                {
+                    settings.Append(ENV_VARS_SEPARATOR);
+                }
+
+                settings.Append(i.Key);
+                settings.Append(ENV_VAR_SEPARATOR);
+                settings.Append(i.Value);
+            }
+
+            Environment.SetEnvironmentVariable(ENV_VAR_NAME, settings.ToString(), EnvironmentVariableTarget.User);
+        }
+
+        override public string ToString()
+        {
+            StringBuilder settings = new();
+
+            foreach (var i in _settings)
+            {
+                settings.Append(i.Key);
+                settings.Append(ENV_VAR_SEPARATOR);
+                settings.Append(i.Value);
+                settings.AppendLine();
+            }
+
+            return settings.ToString();
+        }
+    }
+}
---
+++ Services/Hotels/Hotels.PgSetup/Program.cs
@@ -0,0 +1,171 @@
+﻿namespace Hotels.PgSetup
+{
+    /// <summary>
+    /// 
+    /// Установка необходимых настроек для подключения к БД на PostgreSQL.
+    /// Настройки хранятся в реестре: HKEY_CURRENT_USER/Environment.
+    /// 
+    /// Название папки: Otus-Reservation-hotels
+    /// Настройки:
+    /// - user_id
+    /// - host
+    /// - port
+    /// - password
+    /// - database_name
+    /// 
+    /// </summary>
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PgSettingsManager pgset = new();
+
+            while (true)
+            {
+                Console.WriteLine("Enter an item from the list below.");
+                Console.WriteLine("1. Set user id");
+                Console.WriteLine("2. Set host");
+                Console.WriteLine("3. Set port");
+                Console.WriteLine("4. Set password");
+                Console.WriteLine("5. Set database name");
+                Console.WriteLine("--------------------");
+                Console.WriteLine("6. Read settings");
+                Console.WriteLine("7. Save settings");
+                Console.WriteLine("8. Show settings");
+                Console.WriteLine("--------------------");
+                Console.WriteLine("0. Quit");
+                Console.WriteLine();
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                Console.Clear();
+
+                switch (keyInfo.KeyChar)
+                {
+                    case '1':
+                        {
+                            Console.WriteLine("Enter a user id:");
+                            string? userid = Console.ReadLine();
+
+                            if (string.IsNullOrEmpty(userid))
+                            {
+                                Console.WriteLine("The string is empty. Enter a non-empty text.");
+                                Console.ReadKey();
+
+                                continue;
+                            }
+
+                            pgset.SetUserId(userid);
+                            break;
+                        }
+                    case '2':
+                        {
+                            Console.WriteLine("Enter a host:");
+                            string? host = Console.ReadLine();
+
+                            if (string.IsNullOrEmpty(host))
+                            {
+                                Console.WriteLine("The string is empty. Enter a non-empty text.");
+                                Console.ReadKey();
+
+                                continue;
+                            }
+
+                            pgset.SetHost(host);
+                            break;
+                        }
+                    case '3':
+                        {
+                            Console.WriteLine("Enter a port:");
+                            string? sport = Console.ReadLine();
+
+                            if (string.IsNullOrEmpty(sport))
+                            {
+                                Console.WriteLine("The string is empty. Enter a non-empty text.");
+                                Console.ReadKey();
+
+                                continue;
+                            }
+
+                            int port = 0;
+                            if (!int.TryParse(sport, out port))
+                            {
+                                Console.WriteLine("The port is non-integer. Enter an integer value.");
+                                Console.ReadKey();
+
+                                continue;
+                            }
+                                
+
+                            pgset.SetUserId(port.ToString());
+                            break;
+                        }
+                    case '4':
+                        {
+                            Console.WriteLine("Enter a password:");
+                            string? psw = Console.ReadLine();
+
+                            if (string.IsNullOrEmpty(psw))
+                            {
+                                pgset.SetPassword(string.Empty);
+                            }
+                            else
+                            {
+                                pgset.SetPassword(psw);
+                            }
+                            
+                            break;
+                        }
+                    case '5':
+                        {
+                            Console.WriteLine("Enter a database name:");
+                            string? name = Console.ReadLine();
+
+                            if (string.IsNullOrEmpty(name))
+                            {
+                                Console.WriteLine("The string is empty. Enter a non-empty text.");
+                                Console.ReadKey();
+
+                                continue;
+                            }
+
+                            pgset.SetDatabaseName(name);
+
+                            break;
+                        }
+                    case '8':
+                        {
+                            Console.WriteLine(pgset.ToString());
+                            Console.ReadLine();
+
+                            break;
+                        }
+                    case '7':
+                        {
+                            pgset.WriteSettings();
+                            Console.WriteLine("Settings are written");
+                            Console.ReadLine();
+
+                            break;
+                        }
+                    case '6':
+                        {
+                            pgset.ReadSettings();
+                            Console.WriteLine("Readed settings are:");
+                            Console.WriteLine(pgset.ToString());
+                            Console.ReadLine();
+
+                            break;
+                        }
+                    default:
+                        {
+                            return;
+                        }
+                }
+
+                Console.Clear();
+            }
+        }
+    }
+}
