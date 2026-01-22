6ba26569159853dc4f2587b648c8b3abbe5ceebd
Ximelz <gerasim282@yandex.ru>
2026-01-22 19:49:27 +0300

    Удалил ошибочно сохраненные классы

--- Services/ReservService/DbInfrastructure/DbContext/ReservDbContext.cs
+++ Services/ReservService/DbInfrastructure/DbContext/ReservDbContext.cs
@@ -22,20 +22,3 @@ namespace ReservService
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql(dbConn);
     }
 }
-
-
-public class T1
-{
-    public int Id { get; set; }
-    public T2 t2 { get; set; }
-}
-
-public class T2
-{
-    public int Id { get; set; }
-    public T3 t3 { get; set; }
-}
-public class T3
-{
-    public int Id { get; set; }
-}
